6f02367db0dfefbcd658b2ac16286ea84e8db0ac
Maxime Sermet <sermet.maxime@lyceepergaud.fr>
2024-02-14 20:20:50 +0100

    modifs projet-cinema index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <?php
 // Récupérer la liste des étudiants dans la table film
 
@@ -40,28 +39,25 @@ $films = $requete->fetchAll(PDO::FETCH_ASSOC);
 <body>
 <!--    Barre de navigation-->
 <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary navbar-expand-md">
+    <nav class="navbar navbar-expand-lg bg-warning navbar-expand-md">
         <div class="container-fluid">
-            <a class="navbar-brand text-success fs-3" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-spotify me-2" viewBox="0 0 16 16">
-                    <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.669 11.538a.5.5 0 0 1-.686.165c-1.879-1.147-4.243-1.407-7.028-.77a.499.499 0 0 1-.222-.973c3.048-.696 5.662-.397 7.77.892a.5.5 0 0 1 .166.686m.979-2.178a.624.624 0 0 1-.858.205c-2.15-1.321-5.428-1.704-7.972-.932a.625.625 0 0 1-.362-1.194c2.905-.881 6.517-.454 8.986 1.063a.624.624 0 0 1 .206.858m.084-2.268C10.154 5.56 5.9 5.419 3.438 6.166a.748.748 0 1 1-.434-1.432c2.825-.857 7.523-.692 10.492 1.07a.747.747 0 1 1-.764 1.288"/>
-                </svg>Spotify</a>
+            <h1 class="navbar-brand text-dark fs-3" href="#"><i class="bi bi-film me-2"></i>Films</h1>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
             <div class="collapse navbar-collapse" id="navbarText">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0 justify-content-end flex-grow-1 pe-3">
                     <li class="nav-item">
-                        <a class="nav-link active fs-5" aria-current="page" href="#">A propos des abonnements</a>
+                        <a class="nav-link active fs-5 fw-semibold" aria-current="page" href="#">Nouveau film</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active fs-5" aria-current="page" href="#">Avis</a>
+                        <a class="nav-link active fs-5 fw-semibold" aria-current="page" href="#">Cinémathèque</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active fs-5" aria-current="page" href="#">Contact</a>
+                        <a class="nav-link active fs-5 fw-semibold" aria-current="page" href="#">Les mieux notés</a>
                     </li>
                 </ul>
-                <button class="btn btn-success" type="submit">Abonnez vous</button>
+                <button type="button" class="btn btn-outline-dark fw-bold border-2">En savoir plus</button>
             </div>
         </div>
     </nav>
@@ -72,11 +68,11 @@ $films = $requete->fetchAll(PDO::FETCH_ASSOC);
         <div class="row">
             <?php foreach ($films as $film) : ?>
                 <div class="col-sm-3">
-                    <div class="card border-dark mb-5 text-center" style="width: 18rem;">
+                    <div class="card border-dark mb-5 text-center border-2" style="width: 18rem;">
                         <div class="card-body">
                             <img src="<?= $film["image"] ?>" alt="">
-                            <p class="mt-3"><?= $film["titre"] ?></p>
-                            <p><?= $film["duree"]?> minutes</p>
+                            <p class="mt-3 fw-semibold fs-5"><?= $film["titre"] ?></p>
+                            <p class="fw-bold"><?= $film["duree"]?> minutes</p>
                             <button class="btn btn-warning">Détails du film</button>
                         </div>
                     </div>
