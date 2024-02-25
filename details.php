47d051648b798d5f564d095498c345470e1c29a0
Maxime Sermet <sermet.maxime@lyceepergaud.fr>
2024-02-25 15:05:19 +0100

    fin du projet

--- details.php
+++ details.php
@@ -22,14 +22,6 @@ $films = $requete->fetchAll(PDO::FETCH_ASSOC);
 $titre = null;
 if (isset($_GET["titre"])) {
     $titre = $_GET["titre"];
-
-foreach ($films as $film) {
-    if ($titre){
-        if ($film["titre"]==$titre) {
-            $image=$film["image"];
-        }
-    }
-}
 }
 ?>
 
@@ -50,39 +42,43 @@ foreach ($films as $film) {
 <!--Insertion d'un menu-->
 <?php include_once './_partials/menu.php' ?>
 
-<div class="text-center">
-<h1 class="badge text-bg-dark text-wrap text-center m-5 fs-1" style="width: 50rem;"><?= $titre ?></h1>
-</div>
-
 <div class="container">
-    <table class="table table-warning table-striped table-bordered border-black text-center border-4">
-        <thead class="table-secondary border-black border-2">
-        <tr>
-            <th>Image</th>
-            <th>Durée</th>
-            <th>Résumé</th>
-            <th>Date de sortie</th>
-            <th>Pays</th>
-        </tr>
-        </thead>
-        <tbody class="border-black border-2">
+    <?php if ($titre): ?>
         <?php foreach ($films as $film) : ?>
-            <?php if ($titre): ?>
-                <?php if ($film["titre"]==$titre): ?>
-                    <tr>
-                        <td><img src="<?= $film["image"] ?>" alt=""></td>
-                        <td><?= $film["duree_heure"] ?></td>
-                        <td><?= $film["resume"]?></td>
-                        <td><?= $film["date_fr"] ?></td>
-                        <td><?= $film["pays"] ?></td>
-                    </tr>
-                <?php endif; ?>
-            <?php else : ?>
-                <p>Les paramètres nom et prénom sont obligatoires</p>
+            <?php if ($film["titre"]==$titre): ?>
+                <div class="text-center">
+                    <h1 class="badge text-bg-dark text-wrap text-center m-5 fs-1" style="width: 50rem;"><?= $titre ?></h1>
+                </div>
+                <table class="table table-warning table-striped table-bordered border-black text-center border-4">
+                    <thead class="table-secondary border-black border-2">
+                        <tr>
+                            <th>Image</th>
+                            <th>Durée</th>
+                            <th>Résumé</th>
+                            <th>Date de sortie</th>
+                            <th>Pays</th>
+                        </tr>
+                    </thead>
+                    <tbody class="border-black border-2">
+                            <tr>
+                                <td><img src="<?= $film["image"] ?>" alt=""></td>
+                                <td><?= $film["duree_heure"] ?></td>
+                                <td><?= $film["resume"]?></td>
+                                <td><?= $film["date_fr"] ?></td>
+                                <td><?= $film["pays"] ?></td>
+                            </tr>
+                    </tbody>
+                </table>
             <?php endif; ?>
         <?php endforeach; ?>
-        </tbody>
-    </table>
+    <?php else : ?>
+        <div class="m-5 text-center text-bg-danger">
+            <h1>Le paramètre titre est obligatoire.</h1>
+            <svg xmlns="http://www.w3.org/2000/svg" width="150" height="150" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
+                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+            </svg>
+        </div>
+    <?php endif; ?>
 </div>
 
 
