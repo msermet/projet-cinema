12d0a4465566dd06901bd55b5fc5a5b2cf3100c9
Maxime Sermet <maximesermet075@gmail.com>
2024-03-04 10:01:32 +0100

    modifs sur index.php

--- details.php
+++ details.php
@@ -73,7 +73,7 @@ if (isset($_GET["titre"])) {
         <?php endforeach; ?>
     <?php else : ?>
         <div class="m-5 text-center text-bg-danger">
-            <h1>Le paramètre titre est obligatoire.</h1>
+            <h1>Film introuvable.</h1>
             <svg xmlns="http://www.w3.org/2000/svg" width="150" height="150" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
                 <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
             </svg>
