0cb998bf4ffe2f2112de0a541b948b53bb9ca2ed
Maxime Sermet <maximesermet075@gmail.com>
2024-04-08 17:54:23 +0200

    debut commentaire film

---
+++ public/ajout-commentaire.php
@@ -0,0 +1,196 @@
+<?php
+require_once '../base.php';
+require_once BASE_PROJET.'/src/database/film-db.php';
+
+session_start();
+if (empty($_SESSION)) {
+    header("Location: ../index.php");
+}
+$pseudo = null;
+if (isset($_SESSION["pseudo"])) {
+    $pseudo= $_SESSION["pseudo"];
+}
+$id_utilisateur = null;
+if (isset($_SESSION['id_utilisateur'])) {
+    $id_utilisateur= $_SESSION['id_utilisateur'];
+}
+
+require_once BASE_PROJET.'/src/database/film-db.php';
+$id = null;
+$erreur=false;
+if (isset($_GET["id"])) {
+    $id = filter_var($_GET["id"],FILTER_VALIDATE_INT);
+    $film = getDetails($id);
+    if ($film==null) {
+        $erreur=true;
+    }
+} else {
+    $erreur=true;
+}
+
+require_once '../base.php';
+require_once BASE_PROJET.'/src/database/film-db.php';
+require_once BASE_PROJET.'/src/database/utilisateur-db.php';
+require_once BASE_PROJET."/src/fonctions.php";
+?>
+
+<?php
+// Déterminer si le formulaire a été soumis
+// Utilisation d'une variable superglobale $_SERVER
+// $_SERVER : tableau associatif contenant des informations sur la requête HTTP
+$erreurs = [];
+$titre = "";
+$resume = "";
+$duree = "";
+$date = "";
+$pays = "";
+$image = "";
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Le formulaire a été soumis !
+    // Traiter les données du formulaire
+    // Récupérer les valeurs saisies par l'utilisateur
+    // Superglobale $_POST : tableau associatif
+    $titre = $_POST['titre'];
+    $resume = $_POST['resume'];
+    $duree = $_POST['duree'];
+    $date = $_POST['date'];
+    $pays = $_POST['pays'];
+    $image = $_POST['image'];
+
+    //Validation des données
+    if (empty($titre)) {
+        $erreurs['titre'] = "Le titre est obligatoire";
+    }
+    if (empty($resume)) {
+        $erreurs['resume'] = "Le résumé est obligatoire";
+    }
+    if (empty($duree)) {
+        $erreurs['duree'] = "La durée est obligatoire";
+    } elseif ($duree<0) {
+        $erreurs['duree'] = "La durée n'est pas valide";
+    }
+    if (empty($date)) {
+        $erreurs['date'] = "La date est obligatoire";
+    }
+    if (empty($pays)) {
+        $erreurs['pays'] = "Le pays est obligatoire";
+    }
+    if (empty($image)) {
+        $image="https://placehold.co/220x305/?text=".$titre;
+    } elseif (!filter_var($image,FILTER_VALIDATE_URL,FILTER_FLAG_PATH_REQUIRED)) {
+        $erreurs['image'] = "Le lien de l'image n'est pas valide";
+    }
+
+    // Traiter les données
+    if (empty($erreurs)) {
+        // Traitement des données (insertion dans une base de données)
+        // Rediriger l'utilisateur vers une autre page du site
+
+        postFilm($titre,$duree,$resume,$date,$pays,$image,$id_utilisateur);
+
+        // Rediriger l'utilisateur vers une autre page du site
+        header("Location: ../index.php");
+        exit();
+    }
+}
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Gluten:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <title>Cinéma - Ajouter un film</title>
+    <link rel="shortcut icon" href="assets/images/camera-reels.svg" />
+</head>
+<body class="bg-light">
+<!--Insertion d'un menu-->
+<?php require_once BASE_PROJET.'/src/_partials/header.php' ?>
+<div class="container">
+    <h1 class="border-bottom border-3 border-primary pt-5">Votre évaluation à propos de : <?= $film["titre"] ?></h1>
+    <div class="w-50 mx-auto shadow my-5 p-4 bg-white rounded-5">
+        <form action="" method="post" novalidate>
+            <div class="mb-3">
+                <label for="titre" class="form-label fw-semibold">Titre*</label>
+                <input type="text"
+                       class="form-control <?= (isset($erreurs['titre'])) ? "border border-2 border-danger" : "" ?>"
+                       id="titre" name="titre" value="<?= $titre ?>" placeholder="Saisir le titre"
+                       aria-describedby="emailHelp">
+                <?php if (isset($erreurs['titre'])) : ?>
+                    <p class="form-text text-danger"><?= $erreurs['titre'] ?></p>
+                <?php endif; ?>
+            </div>
+            <div class="mb-3">
+                <label for="resume" class="form-label fw-semibold">Résumé*</label>
+                <input type="text"
+                       class="form-control <?= (isset($erreurs['resume'])) ? "border border-2 border-danger" : "" ?>"
+                       id="resume"
+                       name="resume" value="<?= $resume ?>" placeholder="Saisir le résumé"
+                       aria-describedby="emailHelp">
+                <?php if (isset($erreurs['resume'])) : ?>
+                    <p class="form-text text-danger"><?= $erreurs['resume'] ?></p>
+                <?php endif; ?>
+            </div>
+            <div class="mb-3">
+                <label for="duree" class="form-label fw-semibold">Durée*</label>
+                <input type="number"
+                       class="form-control <?= (isset($erreurs['duree'])) ? "border border-2 border-danger" : "" ?>"
+                       id="duree" name="duree" value="<?= $duree ?>" placeholder="Saisir la durée"
+                       aria-describedby="emailHelp">
+                <?php if (isset($erreurs['duree'])) : ?>
+                    <p class="form-text text-danger"><?= $erreurs['duree'] ?></p>
+                <?php endif; ?>
+            </div>
+            <div class="mb-3">
+                <label for="date" class="form-label fw-semibold">Date*</label>
+                <input type="date"
+                       class="form-control <?= (isset($erreurs['date'])) ? "border border-2 border-danger" : "" ?>"
+                       id="date" name="date" value="<?= $date ?>" placeholder="Saisir la date de sortie"
+                       aria-describedby="emailHelp">
+                <?php if (isset($erreurs['date'])) : ?>
+                    <p class="form-text text-danger"><?= $erreurs['date'] ?></p>
+                <?php endif; ?>
+            </div>
+            <div class="mb-3">
+                <label for="pays" class="form-label fw-semibold">Pays*</label>
+                <input type="text"
+                       class="form-control <?= (isset($erreurs['pays'])) ? "border border-2 border-danger" : "" ?>"
+                       id="pays" name="pays" value="<?= $pays ?>" placeholder="Saisir le pays"
+                       aria-describedby="emailHelp">
+                <?php if (isset($erreurs['pays'])) : ?>
+                    <p class="form-text text-danger"><?= $erreurs['pays'] ?></p>
+                <?php endif; ?>
+            </div>
+            <div class="mb-3">
+                <label for="image" class="form-label fw-semibold">Image*</label>
+                <input type="url"
+                       class="form-control <?= (isset($erreurs['image'])) ? "border border-2 border-danger" : "" ?>"
+                       id="image" name="image" value="<?= $image ?>" placeholder="Saisir le lien de l'image"
+                       aria-describedby="emailHelp">
+                <?php if (isset($erreurs['image'])) : ?>
+                    <p class="form-text text-danger"><?= $erreurs['image'] ?></p>
+                <?php endif; ?>
+            </div>
+            <div class="alert alert-info text-center text-black border-dark" role="alert">
+                <i class="bi bi-info-circle-fill me-2"></i>Une image sera générée automatiquement si vous ne saisissez pas de lien pour l'image du film !
+            </div>
+            <div class="text-center pt-2">
+                <button type="submit" class="btn btn-primary">Ajouter</button>
+            </div>
+        </form>
+    </div>
+</div>
+
+
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
--- public/details.php
+++ public/details.php
@@ -45,9 +45,10 @@ if (isset($_GET["id"])) {
 <!--Insertion d'un menu-->
 <?php require_once BASE_PROJET.'/src/_partials/header.php' ?>
 
-<section class="container bg-white shadow-lg p-3 mb-5 bg-white rounded my-5">
+<!--Details-->
+<section class=" container">
     <?php if (!$erreur): ?>
-        <div class="container">
+        <div class="bg-white shadow-lg p-3 mb-5 rounded my-5">
             <h1 class="border-bottom border-primary border-3">Détails</h1>
             <div class="row align-items-center">
                 <div class="col text-center">
@@ -65,12 +66,31 @@ if (isset($_GET["id"])) {
                     <p><span class="me-2 fst-italic fw-semibold">Synopsis:</span><?= $film["resume"]?></p>
                     <p class="text-secondary">Film créé par : <?php $pseudo=getPseudoUtilisateur($film["id_utilisateur"]);
                         echo $pseudo['pseudo_utilisateur']?></p>
-
                 </div>
             </div>
         </div>
+
+        <!--Commentaires-->
+        <section>
+            <?php if (!$erreur): ?>
+                <div class="bg-white shadow-lg p-3 mb-5 bg-white rounded">
+                    <div class="row">
+                        <div class="col-xl-9 col-md-8 col-sm-7 col-xs-6">
+                            <h1>Commentaires</h1>
+                        </div>
+                        <div class="col-xl-3 col-md-4 col-sm-5">
+                            <?php if (isset($_SESSION['pseudo'])) : ?>
+                                <a class="btn btn-info text-dark fw-semibold mb-2" href="ajout-commentaire.php?id=<?= $film["id_film"] ?>" role="button">Ajouter un commentaire</a>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                    <div class="border-bottom border-primary border-3"></div>
+                </div>
+            <?php endif; ?>
+        </section>
+
     <?php else : ?>
-        <div class="m-5 text-center text-bg-danger">
+        <div class=" text-center text-bg-danger shadow-lg rounded mt-5">
             <h1>Film introuvable...</h1>
             <svg xmlns="http://www.w3.org/2000/svg" width="150" height="150" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
                 <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
@@ -78,7 +98,6 @@ if (isset($_GET["id"])) {
         </div>
     <?php endif; ?>
 </section>
-
 
 <script src="assets/js/bootstrap.bundle.min.js"></script>
 </body>
