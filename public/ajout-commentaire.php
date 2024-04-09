9917981de7f6b9c407eac543fa646156e124f090
Maxime Sermet <maximesermet075@gmail.com>
2024-04-09 09:48:00 +0200

    debut commentaire film

--- public/ajout-commentaire.php
+++ public/ajout-commentaire.php
@@ -52,42 +52,19 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // Récupérer les valeurs saisies par l'utilisateur
     // Superglobale $_POST : tableau associatif
     $titre = $_POST['titre'];
-    $resume = $_POST['resume'];
-    $duree = $_POST['duree'];
-    $date = $_POST['date'];
-    $pays = $_POST['pays'];
-    $image = $_POST['image'];
 
     //Validation des données
     if (empty($titre)) {
         $erreurs['titre'] = "Le titre est obligatoire";
     }
-    if (empty($resume)) {
-        $erreurs['resume'] = "Le résumé est obligatoire";
-    }
-    if (empty($duree)) {
-        $erreurs['duree'] = "La durée est obligatoire";
-    } elseif ($duree<0) {
-        $erreurs['duree'] = "La durée n'est pas valide";
-    }
-    if (empty($date)) {
-        $erreurs['date'] = "La date est obligatoire";
-    }
-    if (empty($pays)) {
-        $erreurs['pays'] = "Le pays est obligatoire";
-    }
-    if (empty($image)) {
-        $image="https://placehold.co/220x305/?text=".$titre;
-    } elseif (!filter_var($image,FILTER_VALIDATE_URL,FILTER_FLAG_PATH_REQUIRED)) {
-        $erreurs['image'] = "Le lien de l'image n'est pas valide";
-    }
+
 
     // Traiter les données
     if (empty($erreurs)) {
         // Traitement des données (insertion dans une base de données)
         // Rediriger l'utilisateur vers une autre page du site
 
-        postFilm($titre,$duree,$resume,$date,$pays,$image,$id_utilisateur);
+        //postFilm($titre);
 
         // Rediriger l'utilisateur vers une autre page du site
         header("Location: ../index.php");
@@ -116,72 +93,29 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <!--Insertion d'un menu-->
 <?php require_once BASE_PROJET.'/src/_partials/header.php' ?>
 <div class="container">
-    <h1 class="border-bottom border-3 border-primary pt-5">Votre évaluation à propos de : <?= $film["titre"] ?></h1>
+    <h1 class="border-bottom border-3 border-primary pt-5">Votre évaluation pour <span class="fw-semibold fst-italic"><?= $film["titre"] ?></span></h1>
     <div class="w-50 mx-auto shadow my-5 p-4 bg-white rounded-5">
         <form action="" method="post" novalidate>
             <div class="mb-3">
                 <label for="titre" class="form-label fw-semibold">Titre*</label>
                 <input type="text"
                        class="form-control <?= (isset($erreurs['titre'])) ? "border border-2 border-danger" : "" ?>"
-                       id="titre" name="titre" value="<?= $titre ?>" placeholder="Saisir le titre"
+                       id="titre" name="titre" value="<?= $titre ?>" placeholder="Saisir un titre"
                        aria-describedby="emailHelp">
                 <?php if (isset($erreurs['titre'])) : ?>
                     <p class="form-text text-danger"><?= $erreurs['titre'] ?></p>
                 <?php endif; ?>
             </div>
             <div class="mb-3">
-                <label for="resume" class="form-label fw-semibold">Résumé*</label>
-                <input type="text"
-                       class="form-control <?= (isset($erreurs['resume'])) ? "border border-2 border-danger" : "" ?>"
-                       id="resume"
-                       name="resume" value="<?= $resume ?>" placeholder="Saisir le résumé"
-                       aria-describedby="emailHelp">
-                <?php if (isset($erreurs['resume'])) : ?>
-                    <p class="form-text text-danger"><?= $erreurs['resume'] ?></p>
-                <?php endif; ?>
+                <label for="exampleFormControlTextarea1" class="form-label fw-semibold">Avis*</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Saisir votre avis"></textarea>
             </div>
             <div class="mb-3">
-                <label for="duree" class="form-label fw-semibold">Durée*</label>
+                <label for="duree" class="form-label fw-semibold">Votre note*</label>
                 <input type="number"
                        class="form-control <?= (isset($erreurs['duree'])) ? "border border-2 border-danger" : "" ?>"
-                       id="duree" name="duree" value="<?= $duree ?>" placeholder="Saisir la durée"
-                       aria-describedby="emailHelp">
-                <?php if (isset($erreurs['duree'])) : ?>
-                    <p class="form-text text-danger"><?= $erreurs['duree'] ?></p>
-                <?php endif; ?>
-            </div>
-            <div class="mb-3">
-                <label for="date" class="form-label fw-semibold">Date*</label>
-                <input type="date"
-                       class="form-control <?= (isset($erreurs['date'])) ? "border border-2 border-danger" : "" ?>"
-                       id="date" name="date" value="<?= $date ?>" placeholder="Saisir la date de sortie"
-                       aria-describedby="emailHelp">
-                <?php if (isset($erreurs['date'])) : ?>
-                    <p class="form-text text-danger"><?= $erreurs['date'] ?></p>
-                <?php endif; ?>
-            </div>
-            <div class="mb-3">
-                <label for="pays" class="form-label fw-semibold">Pays*</label>
-                <input type="text"
-                       class="form-control <?= (isset($erreurs['pays'])) ? "border border-2 border-danger" : "" ?>"
-                       id="pays" name="pays" value="<?= $pays ?>" placeholder="Saisir le pays"
-                       aria-describedby="emailHelp">
-                <?php if (isset($erreurs['pays'])) : ?>
-                    <p class="form-text text-danger"><?= $erreurs['pays'] ?></p>
-                <?php endif; ?>
-            </div>
-            <div class="mb-3">
-                <label for="image" class="form-label fw-semibold">Image*</label>
-                <input type="url"
-                       class="form-control <?= (isset($erreurs['image'])) ? "border border-2 border-danger" : "" ?>"
-                       id="image" name="image" value="<?= $image ?>" placeholder="Saisir le lien de l'image"
-                       aria-describedby="emailHelp">
-                <?php if (isset($erreurs['image'])) : ?>
-                    <p class="form-text text-danger"><?= $erreurs['image'] ?></p>
-                <?php endif; ?>
-            </div>
-            <div class="alert alert-info text-center text-black border-dark" role="alert">
-                <i class="bi bi-info-circle-fill me-2"></i>Une image sera générée automatiquement si vous ne saisissez pas de lien pour l'image du film !
+                       id="duree" name="duree" value="<?= $duree ?>" placeholder="Saisir votre note entre 0 et 5"
+                       aria-describedby="emailHelp" min="0" max="5">
             </div>
             <div class="text-center pt-2">
                 <button type="submit" class="btn btn-primary">Ajouter</button>
